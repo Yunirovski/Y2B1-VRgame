76819fb81db8ce77137697738c68d6cd083b1cc7
Yuni <46707148+Yunirovski@users.noreply.github.com>
2025-09-29 00:17:14 +0200

    Added customer behavior

---
+++ My project/Assets/scripts/customer move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CustomerMove : MonoBehaviour
+{
+    public Transform whereToGo; // Where the customer walks to
+    public float speed = 1f;    // Walk speed
+
+    void Update()
+    {
+        // Walk to the target
+        if (whereToGo != null)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                whereToGo.position,
+                speed * Time.deltaTime
+            );
+        }
+    }
+}
