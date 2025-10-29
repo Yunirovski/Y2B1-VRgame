05d251a6e05d3435a2877ee915373169b41e7cd3
LilyFuyu <milanrijks@gmail.com>
2025-10-29 17:13:47 +0100

    trash chute

---
+++ My project/Assets/Scripts/TrashChute.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrashChute : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    public UnityEvent OnEnterTrigger;
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
